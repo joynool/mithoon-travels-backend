58ac06fe6081a1a0bd6b45ab82fd4ef3fd802982
JoynoolAbedinMithoon <mithun55@gmail.com>
2022-01-26 19:59:54 +0600

    database connected successfully

--- index.js
+++ index.js
@@ -9,30 +9,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iganj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }); */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iganj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run ()
 {
     try {
-        /* await client.connect();
+        await client.connect();
         const database = client.db('mithoon-travels');
-        const rulesCollect = database.collection('rules');
-        const committeeCollect = database.collection('committee');
+        const blogsCollect = database.collection('blogs');
 
-        app.get('/rules', async (req, res) =>
-        {
-            const cursor = rulesCollect.find({});
-            const result = await cursor.toArray();
-            res.send(result);
-        });
-
-        app.get('/committee', async (req, res) =>
-        {
-            const cursor = committeeCollect.find({});
-            const result = await cursor.toArray();
-            res.send(result);
-        }); */
     }
     finally {
         //await client.close();
