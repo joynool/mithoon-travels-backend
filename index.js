09865337ed6883c49dd89206a58fb49ddfdf3e58
JoynoolAbedinMithoon <mithun55@gmail.com>
2022-01-28 14:35:41 +0600

    find one blog api done

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run ()
             });
         });
 
+        app.get('/blogs/:id', async (req, res) =>
+        {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await blogsCollect.findOne(query);
+            res.send(result);
+        });
+
         app.get('/spot', async (req, res) =>
         {
             const cursor = spotCollect.find({});
